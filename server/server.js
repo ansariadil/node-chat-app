f8dbcc2d2a1b2c62a896e9afb058ebf8e8e222da
Adil Ansari <ansariadil81@gmail.com>
2018-07-13 16:07:09 +0530

    connection

--- server/server.js
+++ server/server.js
@@ -1,14 +1,21 @@
 const path = require('path')
 const express = require('express')
+const socketIO = require('socket.io')
+const http = require('http')
 
 const publicPath = path.join(__dirname, '..','public')
 // const port = process.env.PORT ||3000
-
 let app = express()
+let server = http.createServer(app)
+let io = socketIO(server)
 
 app.use(express.static(publicPath));
 
+io.on('connection', (socket) =>{
+    console.log('New USerConnected')
+})
 
-app.listen(process.env.PORT ||3000, function(){
+
+server.listen(process.env.PORT ||3000, function(){
     console.log(`Server is up at port ${this.address().port} in ${app.settings.env} mode`)
 })
