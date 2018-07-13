1f5db9ff4e42d69f70ef284e4510b6d7fd885bf1
Adil Ansari <ansariadil81@gmail.com>
2018-07-13 15:07:50 +0530

    port

--- server/server.js
+++ server/server.js
@@ -2,13 +2,13 @@ const path = require('path')
 const express = require('express')
 
 const publicPath = path.join(__dirname, '..','public')
-const port = process.env.PORT ||3000
+// const port = process.env.PORT ||3000
 
 let app = express()
 
 app.use(express.static(publicPath));
 
 
-app.listen(port, () => {
-    console.log(`Server is up at port ${port}`)
+app.listen(process.env.PORT ||3000, function(){
+    console.log(`Server is up at port ${this.address().port} in ${app.settings.env} mode`)
 })
